2b494d94dca05058a89781fa7bb36da21b28c883
Sabiq Khan <sabiq.am.khan@gmail.com>
2024-06-29 16:00:12 -0700

    refactor: Created subdirectories for pages and components

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-fetch("navbar.html").then(response => response.text()).then(
+fetch("../../components/navbar.html").then(response => response.text()).then(
     data => {
         document.getElementById("navbar").innerHTML = data;
     }
 ).catch(error => console.error("Error:", error));
 
-fetch("footer.html").then(response => response.text()).then(
+fetch("../../components/footer.html").then(response => response.text()).then(
     data => {
         document.getElementById("footer").innerHTML = data;
     }
